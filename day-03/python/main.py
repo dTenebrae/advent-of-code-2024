a58f1fcf43d7b969a7de68a6be67a26f7839b794
Artem Chernyshev <artem.chernyshev@red-soft.ru>
2024-12-03 18:09:34 +0300

    working part 1-2 day 3 python

--- day-03/python/main.py
+++ day-03/python/main.py
@@ -15,8 +15,8 @@ class Solution:
 
     def __init__(self, file_path='../input.txt'):
         self.data = self.input_open(file_path)
-        self.re = re.compile(r"mul\((\d+),(\d+)\)", re.MULTILINE)
-        self.test_str = r"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+        self.re_first = re.compile(r"mul\((\d+),(\d+)\)", re.MULTILINE)
+        self.re_second = re.compile(r"mul\(\d+,\d+\)|do\(\)|don't\(\)")
 
     @staticmethod
     def re_mul(lst: list) -> int:
@@ -25,9 +25,40 @@ class Solution:
         return result
 
     def first_calc(self):
-        return self.re_mul(self.re.findall(self.data))
+        return sum([self.re_mul(self.re_first.findall(item)) for item in self.data.split('\n')])
+
+    # def second_calc(self):
+    #     lst = []
+    #     flag = True
+    #     for item in self.data:
+    #         for idx, slc in enumerate(item.split("don't()")):
+    #             if idx == 0 and flag:
+    #                 lst.append(slc)
+    #                 flag = False
+    #                 continue
+    #             if "do()" in slc:
+    #                 lst.append(slc[slc.index("do()"):])
+    #
+    #     return sum([self.re_mul(self.re.findall(item)) for item in lst])
+
+    def second_calc(self):
+        matches = self.re_second.findall(self.data)
+
+        result = 0
+        flag = True
+        for match in matches:
+            if match == "do()":
+                flag = True
+            elif match == "don't()":
+                flag = False
+            elif flag:
+                result += self.re_mul(self.re_first.findall(match))
+            else:
+                continue
+        return result
 
 
 if __name__ == '__main__':
     sol = Solution()
     print(sol.first_calc())
+    print(sol.second_calc())
