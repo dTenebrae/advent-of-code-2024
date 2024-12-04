cc2120c8a81d92db64ae2c6387627f6e2ba9c7c3
tenebrae <dtenebrae@gmail.com>
2024-12-05 00:18:34 +0300

    day 4 part 2 python

--- day-04/python/main.py
+++ day-04/python/main.py
@@ -20,17 +20,17 @@ class Solution:
         return np.pad(np.array(data),
                       [(0, 3), (0, 3)],
                       mode='constant',
-                      constant_values=666)
+                      constant_values=-1)
 
-    def get_windows(self):
-        return np.lib.stride_tricks.sliding_window_view(self.data, window_shape=(4, 4))
+    def get_windows(self, window_shape=(4, 4)):
+        return np.lib.stride_tricks.sliding_window_view(self.data, window_shape=window_shape)
 
     @staticmethod
     def test_seq(seq):
         diff = np.diff(seq)
         return all(diff == 1) or all(diff == -1)
 
-    def cnt_window(self, arr):
+    def cnt_window_first(self, arr):
         return sum(
             (
                 self.test_seq(arr[:, 0]),
@@ -39,14 +39,26 @@ class Solution:
                 self.test_seq(np.diag(np.fliplr(arr)))
         )
 
-    def first_calc(self):
+    def cnt_window_second(self, arr):
+        return self.test_seq(arr.diagonal()) and self.test_seq(np.diag(np.fliplr(arr)))
+
+    def final_count(self, f, window_shape=(4, 4), change_x=False):
         result = 0
-        windows = self.get_windows()
+        if change_x:
+            self.data[self.data == 1] = -10
+
+        windows = self.get_windows(window_shape=window_shape)
         for i in range(0, windows.shape[0]):
             for j in range(0, windows.shape[1]):
                 tst_window = windows[i][j]
-                result += self.cnt_window(tst_window)
+                result += f(tst_window)
         return result
+
+    def first_calc(self):
+        return self.final_count(f=self.cnt_window_first)
+
+    def second_calc(self):
+        return self.final_count(f=self.cnt_window_second, window_shape=(3, 3), change_x=True)
 
     def __init__(self, file_path='../input.txt'):
         self.data = self.input_open(file_path)
@@ -55,3 +67,4 @@ class Solution:
 if __name__ == '__main__':
     sol = Solution()
     print(sol.first_calc())
+    print(sol.second_calc())
