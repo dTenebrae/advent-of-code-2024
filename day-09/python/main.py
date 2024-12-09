ca612c799a513181d051a0b2770ffe25daa700ce
Artem Chernyshev <artem.chernyshev@red-soft.ru>
2024-12-09 17:52:36 +0300

    day 9 part 2 python

--- day-09/python/main.py
+++ day-09/python/main.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 EMPTY = -1
 
 
@@ -17,6 +19,18 @@ class Solution:
     def __init__(self, file_path='../input.txt'):
         self.data = self.input_open(file_path)
 
+    @staticmethod
+    def is_monotonic(seq) -> bool:
+        diff = np.diff(seq)
+        return all(diff == 1) or all(diff == -1)
+
+    @staticmethod
+    def get_free_mem(tr_array: list):
+        all_empty = np.where(np.array(tr_array) == EMPTY)[0]
+        diff = np.diff(all_empty)
+        breaks = np.where(diff != 1)[0] + 1
+        return np.split(all_empty, breaks)
+
     def transform(self):
         file_flag = True
         result = []
@@ -31,7 +45,7 @@ class Solution:
         return result
 
     @staticmethod
-    def compactify(tr_array: list):
+    def compactify_first(tr_array: list):
         end_ptr = len(tr_array) - 1
         for front_ptr in range(0, len(tr_array)):
             if front_ptr >= end_ptr:
@@ -46,13 +60,38 @@ class Solution:
                 break
         return tr_array
 
+    def compactify_second(self, tr_array: list):
+        free_mem = self.get_free_mem(tr_array)
+        arr = np.array(tr_array)
+        max_val = max(arr)
+        for num in range(max_val, -1, -1):
+            where_num = np.where(arr == num)[0]
+            needed_len = len(where_num)
+            # find free space in memory list
+            for i, mem in enumerate(free_mem):
+                if len(mem) < needed_len:
+                    continue
+                if where_num[0] <= mem[0]:
+                    break
+                for idx in range(needed_len):
+                    arr[mem[idx]] = num
+                # Clean up copied values
+                arr[where_num] = EMPTY
+                # Delete used memory from list
+                free_mem[i] = np.delete(free_mem[i], range(needed_len))
+                break
+
+        return arr
+
     @staticmethod
-    def first_calc(cmp_arr: list):
+    def summarize(cmp_arr: list):
         return sum([num * item for num, item in enumerate(cmp_arr) if item != EMPTY])
 
 
 if __name__ == '__main__':
     sol = Solution()
     transformed = sol.transform()
-    compacted = sol.compactify(transformed)
-    print(sol.first_calc(compacted))
+    compacted_first = sol.compactify_first(transformed.copy())
+    print(sol.summarize(compacted_first))
+    compacted_second = sol.compactify_second(transformed)
+    print(sol.summarize(compacted_second))
