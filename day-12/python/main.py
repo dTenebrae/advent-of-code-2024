09221c199108a79b4e1ab3ee81a1f68538926bd0
tenebrae <dtenebrae@gmail.com>
2024-12-14 17:59:34 +0300

    day 12 part 2 python

--- day-12/python/main.py
+++ day-12/python/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+from itertools import groupby
 from scipy.ndimage import label
 
 FILL_VALUE = -1
@@ -44,13 +45,45 @@ class Solution:
     def count_perim(self, num, region):
         result = 0
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        for reg in region:
-            x, y = reg
+        for x, y in region:
             for dx, dy in directions:
                 nx, ny = x + dx, y + dy
                 if self.data[nx, ny] == FILL_VALUE or self.data[nx, ny] != ord(num):
                     result += 1
         return result
+
+    @staticmethod
+    def n_monotonics(seq):
+        seq = [item[0] for item in seq]
+        diff = np.diff(seq)
+        breaks = np.where(diff != 1)[0] + 1
+        return len(np.split(seq, breaks))
+
+    def real_counter(self, left: list, right: list) -> int:
+        result = 0
+        left = sorted(left, key=lambda x: x[1])
+        right = sorted(right, key=lambda x: x[1])
+        split_left = [list(g) for k,g in groupby(left, lambda x: x[1])]
+        split_right = [list(g) for k,g in groupby(right, lambda x: x[1])]
+        for side in [split_left, split_right]:
+            for item in side:
+                result += self.n_monotonics(item)
+        return result * 2
+
+    def count_sides(self, num, region):
+        result = 0
+        left_side = []
+        right_side = []
+        directions = [(0, -1), (0, 1)]
+        for x, y in region:
+            for idx, (dx, dy) in enumerate(directions):
+                nx, ny = x + dx, y + dy
+                if self.data[nx, ny] == FILL_VALUE or self.data[nx, ny] != ord(num):
+                    if idx == 0:
+                        left_side.append((x, y))
+                    else:
+                        right_side.append((x, y))
+        return self.real_counter(left_side, right_side)
 
     def extract_regions(self, value):
         binary_matrix = (self.data == ord(value)).astype(int)
@@ -65,15 +98,26 @@ class Solution:
 
     def first_calc(self):
         result = 0
-        for char in sol.charset:
-            region = self.extract_regions(char)
-            for reg in region:
+        for char in self.charset:
+            regions = self.extract_regions(char)
+            for reg in regions:
                 square = self.count_square(reg)
                 perim = self.count_perim(char, reg)
                 result += square * perim
+        return result
+
+    def second_calc(self):
+        result = 0
+        for char in self.charset:
+            regions = self.extract_regions(char)
+            for reg in regions:
+                square = self.count_square(reg)
+                sides = self.count_sides(char, reg)
+                result += square * sides
         return result
 
 
 if __name__ == '__main__':
     sol = Solution()
     print(sol.first_calc())
+    print(sol.second_calc())
